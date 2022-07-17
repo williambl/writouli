a3943f967fa1df06567afcdd297373f2e8471158
williambl <william@williambl.com>
2022-07-17 20:32:18 +0100

    chore: switch to TS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
+import {AppProps} from "next/app";
 
-function Writouli({ Component, pageProps }) {
+function Writouli({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
