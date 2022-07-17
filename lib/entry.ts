24def192ef7b2a49de1ea515fd3572bc8e166273
williambl <william@williambl.com>
2022-07-17 21:15:16 +0100

    feat: add entry and page types

---
+++ lib/entry.ts
@@ -0,0 +1,8 @@
+import {Page} from "./page";
+
+export type Entry = {
+    name: string,
+    category: string,
+    icon: string,
+    pages: [Page] //TODO
+}
---
+++ lib/page.ts
@@ -0,0 +1,16 @@
+import {string} from "prop-types";
+import {Component} from "react";
+
+export class Page {
+    type: string;
+    advancement: string | null;
+    flag: string | null;
+    anchor: string | null;
+
+    customFields: [PageField<any>]
+}
+
+export abstract class PageField<T> {
+    name: string;
+    editorComponent: ()=>JSX.Element;
+}
