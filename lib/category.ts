7ecaee4668710e830106d31212fb19be4e22678b
williambl <william@williambl.com>
2022-08-28 16:52:45 +0100

    feat: add category

---
+++ lib/category.ts
@@ -0,0 +1,8 @@
+export default class Category {
+    name: string;
+    description: string;
+    icon: string;
+    parent: string;
+    flag: string;
+    secret: boolean;
+}
--- lib/entry.ts
+++ lib/entry.ts
@@ -1,17 +1,17 @@
 import {Page} from "./page";
 import {Book} from "./book";
 import {TranslationContext} from "./translations";
+import Category from "./category";
 
 export class Entry {
     name: string;
-    category: string;
     icon: string;
     pages: Page[];
 
-    toJson(book: Book, translationContext: TranslationContext): any {
+    toJson(category: Category, book: Book, translationContext: TranslationContext): any {
         return {
             'name': translationContext.addTranslation("name", this.name),
-            'category': this.category,
+            'category': category,
             'icon': this.icon,
             'pages': this.pages.map((p, i) => p.toJson(book, translationContext.create(i.toString())))
         }
