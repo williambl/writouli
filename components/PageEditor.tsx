bda71625de68ca1f52408bf9a76e97d89b8cdf31
williambl <william@williambl.com>
2022-09-01 12:49:53 +0100

    fix: wrong layout of page fields

--- components/PageEditor.tsx
+++ components/PageEditor.tsx
@@ -12,9 +12,13 @@ export default function PageEditor(props: {page: Page, setPage: (Page) => void, 
             <LabelledTextInput label="Advancement:" defaultValue={props.page.advancement} onChange={change => props.setPage({...props.page, advancement: change.target.value})} />
             <LabelledTextInput label="Flag:" defaultValue={props.page.flag} onChange={change => props.setPage({ ...props.page, flag: change.target.value })} />
             <LabelledTextInput label="Anchor:" defaultValue={props.page.anchor} onChange={change => props.setPage({ ...props.page, anchor: change.target.value })} />
-            <label>
-                {props.page.customFields.map(field => <div key={field.name}>{field.editorComponent(newData => props.setPage({ ...props.page, customFields: replace(props.page.customFields, (f) => field.name === f.name, {...field, data: newData })}))}</div>)}
-            </label>
+            { props.page.customFields.map(field =>
+                <div key={field.name}>
+                    {
+                        field.editorComponent(newData => props.setPage({ ...props.page, customFields: replace(props.page.customFields, (f) => field.name === f.name, {...field, data: newData })}))
+                    }
+                </div>
+            ) }
         </fieldset>
     )
 }
